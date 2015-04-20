d5eb39d10ecb9b46f731e284b9986dcafa89a4ea
lizheng <lizhenghn@gmail.com>
2015-04-20 19:04:08 +0800

    ...

--- PythonStudy/scrapy/test_urllib/test_urllib.py
+++ PythonStudy/scrapy/test_urllib/test_urllib.py
@@ -8,6 +8,15 @@ import urllib
 import urllib2
 import getpass
 
+def just_visit_url(url):
+    req = urllib2.Request(url)
+    fd = urllib2.urlopen(req)
+    print("get url: %s" % fd.geturl())
+    info = fd.info()
+    for key,value in info.items():
+        print("[header] %s = %s" % (key, value))
+    print(fd.read())
+    
 url_site = "http:///www.python.org/doc/FAQ.html"
 # url_site == urlparse.urlunparse(urlparse.urlparse(url_site))
 tup = urlparse.urlparse(url_site)
@@ -36,30 +45,7 @@ for key,value in info.items():
 print(fd.readline())
 
 
-# 带认证功能的请求
-class TerminalPassword(urllib2.HTTPPasswordMgr):
-    def find_user_password(self, realm, authuri):
-        print("###### TerminalPassword ######")
-        retval = urllib2.HTTPPasswordMgr.find_user_password(self, realm, authuri)
-        if retval[0] == None and retval[1] == None:
-            sys.stdout.write("Login required for %s at %s" & (realm, authuri))
-            sys.stdout.write("Username: ")
-            username = sys.stdin.readline().rstrip()
-            password = getpass.getpass().rstrip()
-            return (username, password)
-        else:
-            return retval
 
-def test_urlauth():
-    url_site = "http://passport.cnblogs.com/user/signin"
-    req = urllib2.Request(url_site)
-    opener = urllib2.build_opener(urllib2.HTTPBasicAuthHandler(TerminalPassword()))
-    fd = opener.open(req)
-    print("get url: %s" % fd.geturl())
-    info = fd.info()
-    for key,value in info.items():
-        print("[header] %s = %s" % (key, value))
-    print(fd.readline())
 
 if __name__ == '__main__':
-    test_urlauth()
+    print("##########################")
